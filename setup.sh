41ffbe5d1486e391a4057a71a4a4003c1e46a5b0
sachin175638 <35227965+sachin175638@users.noreply.github.com>
2018-03-04 17:25:54 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade -y
+apt-get install figlet -y
+apt-get install python2 -y
+cd $HOME/sub
+chmod +x main.py
+chmod +x dss.py
+chmod +x dessign.py
+rm -rf /data/data/com.termux/files/usr/bin/dss
+cd $HOME
+rm -rf dss.sh
+touch dss.sh
+chmod +x dss.sh
+echo "#!/bin/bash" >> dss.sh
+echo "cd $HOME/sub " >> dss.sh
+echo "python2 steel.py" >> dss.sh
+ln -s $HOME/dss.sh /data/data/com.termux/files/usr/bin/dss
+echo
+echo "execute ----dss----"
